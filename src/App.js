8495b389ce9930f7fce157e6c6da215af38c7e09
Phyo Zaw Hein <phyozawhein1997@gmail.com>
2023-06-11 00:46:08 -0400

    finished the test but wanna add more animation and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Card from './Card/card.js'
+import React, {useState,useEffect} from 'react';
 
-function App() {
+const App=()=> {
+const [quotes,setQuotes]= useState([]);
+const [quote,setQuote] = useState("");
+const [author,setAuthor] = useState("");
+const [mainColor,setMainColor]=useState("");
+const fetchQuotes = async ()=>{
+   fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+   .then((res)=> res.json())
+   .then((data)=> {
+    let {quotes}= data
+    setQuotes(quotes)
+    let ranNum = Math.floor(Math.random()*quotes.length);
+    setQuote(quotes[ranNum].quote)
+    setAuthor(quotes[ranNum].author)
+   })
+}
+const fetchNewQuote=()=>{
+  genNewColor();
+  let ranNum = Math.floor(Math.random()*quotes.length);
+  setQuote(quotes[ranNum].quote)
+  setAuthor(quotes[ranNum].author)
+}
+
+
+const genNewColor=()=>{
+  let r = Math.floor(Math.random() * 256); // Random between 0-255
+  let g = Math.floor(Math.random() * 256); // Random between 0-255
+  let b = Math.floor(Math.random() * 256); // Random between 0-255
+  setMainColor( 'rgb(' + r + ',' + g + ',' + b + ')');
+}
+useEffect(()=>{
+  fetchQuotes()
+  genNewColor()
+},[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: mainColor}}>
+      <div className="title">
+        <h2>Random Quote Machine</h2>
+      </div>
+      { quote.length === 0 ? <p>Loading Quotes....</p>:<Card quote={quote} author={author} color={mainColor} getNewQuote={fetchNewQuote}/>}
     </div>
   );
 }
---
+++ src/Card/card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './card.scss';
+
+const card =({quote,author, color,getNewQuote})=>{
+    
+    return(
+        <div id="quote-box" className="cardContainer" >
+            <div className="quoteSec" >
+            <span className="quote" id="text">
+                {quote}
+            </span>
+            <span className="author" id="author">-{author}</span>
+            </div>
+            <div className="quoteButtons">
+                <button id="new-quote" className="newQuote"  onClick={getNewQuote}>New Quote</button>
+                <a className="tweetQuote" id="tweet-quote" href="twitter.com/intent/tweet" >Tweet Quote</a>
+            </div>
+        </div>
+    )
+}
+
+export default card;
